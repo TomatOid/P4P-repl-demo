90751cc3e0b551af498ae54144211ba4705e088e
Connor McCarty <connormc757@gmail.com>
2020-06-25 18:33:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# tell python to look at the numpy libary and call it np
+# numpy is a library for matrix and vector math
+import numpy as np
+# matplotlib plots numpy and python arrays
+import matplotlib.pyplot as plt
+
+# make an array of numbers between -5 and 5 with 0.1 between them
+# x will be like [-5, -4.9, -4.8, ..., 4.8, 4.9, 5]
+x = np.arange(-5, 5, 0.1)
+# now make a plot with our x values determined by our array called x
+# the y is an array of the squares of each element of x
+plt.plot(x, np.square(x))
+# tell matplotlib to open a window with our plot in it
+plt.show()
